a705c43ca24d4b15140d2582c102139e857701fd
MartinVitan <martin.vitan.zz@gmail.com>
2024-12-04 18:06:45 +0100

    ModelsForArchive

--- django_vyvojove_prostredie/archive_app/admin.py
+++ django_vyvojove_prostredie/archive_app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Ensemble, GenreType, Play, Document, EmployeeType, Employee, EmployeeJob, Room, Repeat
 
-# Register your models here.
+admin.site.register(Ensemble)
+admin.site.register(GenreType)
+admin.site.register(Play)
+admin.site.register(Document)
+admin.site.register(EmployeeType)
+admin.site.register(Employee)
+admin.site.register(EmployeeJob)
+admin.site.register(Room)
+admin.site.register(Repeat)
