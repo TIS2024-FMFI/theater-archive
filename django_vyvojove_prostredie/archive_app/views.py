4c29efe2dd5eff6f3084a32ebd09ffbed153c2dc
johanka <jjakubove@gmail.com>
2024-12-11 12:17:41 +0100

    Added main page html code.

--- django_vyvojove_prostredie/archive_app/views.py
+++ django_vyvojove_prostredie/archive_app/views.py
@@ -3,11 +3,12 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def index(response):
-    return HttpResponse('''
-            <h1>Welcome to the Home Page</h1>
-            <button onclick="location.href='/second/'">Go to Second Page</button>
-        ''')
+def index(request):
+    return render(request, 'archive_app/index.html')
+    # return HttpResponse('''
+    #         <h1>Welcome to the Home Page</h1>
+    #         <button onclick="location.href='/second/'">Go to Second Page</button>
+    #     ''')
 
 def second_view(request):
     return HttpResponse('<h1>This is the second page</h1>')
