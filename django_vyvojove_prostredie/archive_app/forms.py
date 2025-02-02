706c55af232150f05c82c4bd2c50ca3e90e7e386
virtualmachine44 <159922657+virtualmachine44@users.noreply.github.com>
2025-02-02 11:00:21 +0100

    minor change

--- django_vyvojove_prostredie/archive_app/forms.py
+++ django_vyvojove_prostredie/archive_app/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Employee
+from .models import Employee, EmployeeJob
 
 class EmployeeForm(forms.ModelForm):
     class Meta:
@@ -7,4 +7,13 @@ class EmployeeForm(forms.ModelForm):
         fields = ['first_name', 'last_name', 'publicity']
         widgets = {
             'publicity': forms.CheckboxInput(),
+        }
+
+class EmployeeJobForm(forms.ModelForm):
+    class Meta:
+        model = EmployeeJob
+        fields = ['job', 'date_start', 'date_end']
+        widgets = {
+            'date_start': forms.DateInput(attrs={'type': 'date'}),
+            'date_end': forms.DateInput(attrs={'type': 'date'}),
         }
