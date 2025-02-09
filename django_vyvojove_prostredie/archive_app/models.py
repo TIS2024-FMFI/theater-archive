3437d99d347f1e0a2bffbe02076672bcf5451b23
virtualmachine44 <bojda2@uniba.sk>
2025-02-09 13:27:12 +0100

    Opravene modely

--- django_vyvojove_prostredie/archive_app/models.py
+++ django_vyvojove_prostredie/archive_app/models.py
@@ -3,12 +3,12 @@ from django.db import models
 
 class Ensemble(models.Model):
     name = models.CharField(max_length=100)
+    foundation_date = models.DateField(null=True)
+    dissolution_date = models.DateField(null=True, blank=True)
     guest = models.BooleanField(default=False) #vm44
     description = models.TextField(max_length=1500, null=True, blank=True) #vm44
-    foundation_date = models.DateField() #co ak nebudu mat info, kedy vznikol subor? nebude lepsie dovolit aj tu null?
-    dissolution_date = models.DateField(null=True, blank=True)
     publicity = models.BooleanField(default=False)
-    
+
     def __str__(self):
         return self.name
 
@@ -28,15 +28,14 @@ class Room(models.Model):
 
 
 class Play(models.Model):
-    ensemble = models.ForeignKey('Ensemble', on_delete=models.CASCADE) # preco nie on_delete=models.SET_NULL ale on_delete=models.CASCADE? trochu nebezpecne
-    genre_type = models.ForeignKey('GenreType', on_delete=models.SET_NULL, null=True, blank=True)
+    title = models.CharField(max_length=200)
     author_first_name = models.CharField(max_length=100, null=True, blank=True)
     author_last_name = models.CharField(max_length=100, null=True, blank=True)
-    title = models.CharField(max_length=200)
-    subtitle = models.CharField(max_length=200, null=True, blank=True) #vm44
-    description = models.TextField(max_length=1500, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
     publicity = models.BooleanField(default=False)
-    
+    ensemble = models.ForeignKey('Ensemble', on_delete=models.CASCADE)
+    genre_type = models.ForeignKey('GenreType', on_delete=models.SET_NULL, null=True, blank=True)
+    subtitle = models.CharField(max_length=200, null=True, blank=True) #vm44
 
     def __str__(self):
         return self.title
@@ -80,12 +79,12 @@ class EmployeeType(models.Model):
         return self.name
 
 
-class Employee(models.Model): # rename to Employees
+class Employee(models.Model):
     id = models.AutoField(primary_key=True)
     ensemble = models.ForeignKey('Ensemble', on_delete=models.SET_NULL, null=True)
     employee_type = models.ForeignKey('EmployeeType', on_delete=models.SET_NULL, null=True)
     genre_type = models.ForeignKey('GenreType', on_delete=models.SET_NULL, null=True) #vm44
-    document = models.ForeignKey('Document', on_delete=models.SET_NULL, null=True, blank=True) #vm44 tabulka Document neexistuje
+    #document = models.ForeignKey('EmployeeDocument', on_delete=models.SET_NULL, null=True, blank=True) #nepotrebujeme
 
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
@@ -93,8 +92,9 @@ class Employee(models.Model): # rename to Employees
     date_of_death = models.DateField(null=True) #vm44
     place_of_birth = models.CharField(max_length=100, null=True, blank=True)
     place_of_death = models.CharField(max_length=100, null=True, blank=True)
-    description = models.TextField(max_length=1500, null=True, blank=True) #vm44   
+    description = models.TextField(null=True, blank=True)    
     publicity = models.BooleanField(default=False) #vm44
+    date_publicity = models.BooleanField(default=False) #vm44
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
@@ -111,7 +111,7 @@ class EmployeeJob(models.Model):
 
 
 class Job(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=50)
     play_character = models.BooleanField(default=False)
 
     def __str__(self):
@@ -119,33 +119,31 @@ class Job(models.Model):
 
 
 class PlayPerformer(models.Model):
-    play = models.ForeignKey('Play', on_delete=models.CASCADE) # zase, preco CASCADE? Nie je to nebezpecne?
-    employee = models.ForeignKey('Employee', null=True, on_delete=models.CASCADE) # nie je v modeli, naco sa to pridalo?
+    play = models.ForeignKey('Play', on_delete=models.CASCADE)
+    employee = models.ForeignKey('Employee', null=True, on_delete=models.CASCADE)
     employee_job = models.ForeignKey('EmployeeJob', on_delete=models.CASCADE)
-    job = models.CharField(max_length=100) # nie je v modeli, naco sa to pridalo?
+    job = models.CharField(max_length=100, null=True)
 
     def __str__(self):
         return f"{self.play} - {self.job}"
 
 
 class Concert(models.Model):
-    id = models.AutoField(primary_key=True) # redundant
-    concert_type = models.ForeignKey('ConcertType', on_delete=models.SET_NULL, null=True)
-    date = models.DateTimeField()
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
-    description = models.TextField(max_length=1500, null=True, blank=True)
+    date = models.DateTimeField()
+    description = models.TextField(null=True, blank=True)
+    concert_type = models.ForeignKey('ConcertType', on_delete=models.SET_NULL, null=True)
     publicity = models.BooleanField(default=False)
-    
 
     def __str__(self):
         return self.name
 
 
 class ConcertPerformer(models.Model):
-    employee_job = models.ForeignKey('EmployeeJob', on_delete=models.CASCADE) #vm44
-    #employee = models.ForeignKey('Employee', on_delete=models.CASCADE) # chyba? nie je v data modeli, alebo sa niekto rozhodol ze to je namiesto employee_job?
     concert = models.ForeignKey('Concert', on_delete=models.CASCADE)
-    job = models.CharField(max_length=50)
+    employee = models.ForeignKey('Employee', on_delete=models.CASCADE)
+    job = models.CharField(max_length=50) 
 
     def __str__(self):
         return f"{self.concert} - {self.job}"
@@ -160,12 +158,12 @@ class ConcertType(models.Model):
 
 class Repeat(models.Model):
     play = models.ForeignKey('Play', on_delete=models.CASCADE)
-    repeat_type = models.ForeignKey('RepeatType', on_delete=models.SET_NULL, null=True)
     room = models.ForeignKey('Room', on_delete=models.SET_NULL, null=True)
-    ensemble = models.ForeignKey('Ensemble', on_delete=models.SET_NULL, null=True) #vm44
-    date = models.DateTimeField()
+    date = models.DateTimeField( )
     publicity = models.BooleanField(default=False)
-    
+    repeat_type = models.ForeignKey('RepeatType', on_delete=models.SET_NULL, null=True)
+    ensemble = models.ForeignKey('Ensemble', on_delete=models.SET_NULL, null=True) #vm44
+
     def __str__(self):
         return f"{self.play} - {self.date}"
 
@@ -179,13 +177,10 @@ class RepeatType(models.Model):
 
 class RepeatPerformer(models.Model):
     repeat = models.ForeignKey('Repeat', on_delete=models.CASCADE)
-    #employee_job = models.ForeignKey('EmployeeJob', on_delete=models.CASCADE) #vm44 chyba? alebo to je akoze rola?
-    employee = models.ForeignKey('Employee', on_delete=models.CASCADE)
-    role = models.CharField(max_length=100) #vm44
+    employee_job = models.ForeignKey('EmployeeJob', on_delete=models.CASCADE) #toto je rola
 
     def __str__(self):
         return f"{self.repeat} - {self.employee_job}"
-
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
